5f3a15013c231e54910afbca9d795482cdca93b8
Quantcom SEA <ckkacc@gmail.com>
2022-02-15 14:17:49 +0800

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { mount } from 'marketing/MarketingApp';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+    return (
+        <div>
+            <h1>Hi there!</h1>
+            <hr />
+            <MarketingApp />
+        </div>
+    )
+
+};
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el) => {
+    ReactDOM.render(<App />, el);
+};
+
+
+// If we are in development and in isolation, 
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root');
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+
+// We are runnin through container
+// and we should export the mount function
+export { mount };
