257db878217b6dd2b42787fcbe0c275c58769e28
Quantcom SEA <ckkacc@gmail.com>
2022-02-15 19:51:10 +0800

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJason = require('../package.json');
@@ -20,9 +19,6 @@ const devConfig = {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
             shared: packageJason.dependencies,
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
         }),
     ],
 };
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJason = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: packageJason.dependencies,
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
