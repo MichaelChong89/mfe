2ecfbf51ac15145517b3881dfce501f65b0223e9
Quantcom SEA <ckkacc@gmail.com>
2022-02-17 05:15:28 +0800

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 // import { mount } from 'marketing/MarketingApp';
 import { BrowserRouter } from 'react-router-dom';
+import {
+    StylesProvider,
+    createGenerateClassName
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <h1>Hi there!!!</h1>
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 };
