73c5baa62326536512fc3cb64964c60c88a5a1fa
Quantcom SEA <ckkacc@gmail.com>
2022-02-16 20:52:28 +0800

    revised header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowerRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
 export default () => {
     return (
-        <BrowerRouter>
+        <BrowserRouter>
             <div>
                 <Header />
                 <MarketingApp />
             </div>
-        </BrowerRouter>
+        </BrowserRouter>
     )
 };
