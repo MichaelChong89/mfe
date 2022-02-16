41d1de16451d7edf0c885251edc6697778936833
Quantcom SEA <ckkacc@gmail.com>
2022-02-16 20:15:42 +0800

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 // import { mount } from 'marketing/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
     return (
-        <div>
-            <h1>Hi there!!!!</h1>
-            <hr />
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     )
 };
